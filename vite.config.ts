f6e9963e5a3e243c52d2ccccb2fe2d9d11530d2b
Bunniesnu <bunniesnu@gmail.com>
2026-09-10 00:19:56 +0900

    Add supabase connection client & simple test

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,13 @@ export default defineConfig({
       "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/supabase": {
+        target: "http://127.0.0.1:54321",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/supabase/, ""),
+      },
+    },
+  },
 })
