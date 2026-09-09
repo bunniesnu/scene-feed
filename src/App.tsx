7c6ab26116d80908edd2777c17ee9dc1e0950b89
Bunniesnu <bunniesnu@gmail.com>
2026-09-10 03:05:41 +0900

    Add @tanstack/router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Page } from '@/pages/home.tsx'
+import { RouterProvider } from '@tanstack/react-router';
+import { router } from '@/router';
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Page />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   )
 }
---
+++ src/routes/__root.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+
+export const Route = createRootRoute({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <Fragment>
+      <Outlet />
+    </Fragment>
+  )
+}
