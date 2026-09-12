d918f1d67b54adade66d135eff3863f4c84a116a
Bunniesnu <bunniesnu@gmail.com>
2026-09-12 20:34:14 +0900

    Add auth status hook & navbar button switch

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,15 @@
 import { Outlet, createRootRoute, Link } from '@tanstack/react-router'
 import { Button } from '@/components/ui/button'
+import { useAuth } from '@/hooks/auth/status';
+import { LogOut } from "lucide-react"
 
 export const Route = createRootRoute({
   component: RootComponent,
 })
 
 function RootComponent() {
+  const { isLoggedIn, loading } = useAuth()
+
   return (
     <div className="min-h-dvh flex flex-col items-center">
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
@@ -14,11 +18,11 @@ function RootComponent() {
             SCENE-feed
           </Link>
           <nav className="flex items-center gap-4 text-sm">
-            <Link to="/login">
+            { isLoggedIn ? <LogOut /> : <Link to="/login">
               <Button variant="outline" size="lg" className="rounded-full cursor-pointer px-4">
                 Sign In
               </Button>
-            </Link>
+            </Link> }
           </nav>
         </div>
       </header>
