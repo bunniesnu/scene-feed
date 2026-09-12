715948384779ff511c65c1bde01ce4b961d3dfe3
Bunniesnu <bunniesnu@gmail.com>
2026-09-13 06:06:09 +0900

    Add isXUrl method

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -10,3 +10,13 @@ export function isInstagramUrl(urlStr: string): boolean {
     return false
   }
 }
+
+export function isXUrl(urlStr: string): boolean {
+  try {
+    const { hostname, pathname } = new URL(urlStr.startsWith("http") ? urlStr : `https://${urlStr}`);
+    const host = hostname.replace(/^www\./, "");
+    return (host === "x.com" || host === "twitter.com") && /^\/[^/]+\/status\/\d+/.test(pathname);
+  } catch {
+    return false;
+  }
+}
